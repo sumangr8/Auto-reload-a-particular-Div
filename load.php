20e5783339fe08353496c54ec303fc86168fafa6
sumangr8 <30518152+sumangr8@users.noreply.github.com>
2018-11-12 12:42:54 +0530

    Add files via upload

---
+++ load.php
@@ -0,0 +1,9 @@
+<?php
+// echo 'This content is loaded via ajax in every 5 seconds ..<br/>';
+// for($i=1;$i <=10;$i++){
+// 	echo $i."<br/>";
+// }
+// echo "load with out refresh";
+?>
+suman kumar singh
+
